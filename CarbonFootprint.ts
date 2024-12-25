2bcd9234d9687a5f849728843afa75795d73bd4e
Purvi Harpalani <purvi.harpalani@tamp.co.in>
2024-12-25 14:35:32 +0530

    Updated variable names of inbox spam and sent for the parameter carbonFootprint

--- CarbonFootprint.ts
+++ CarbonFootprint.ts
@@ -34,22 +34,22 @@ class getCarbonFootprint{
     
     calculateCarbonFootprint(): Record<string, number> {
         const carbonFootprint = {
-            inbox: 0,
-            sent: 0,
-            spam: 0
+            inboxWeightInKG: 0,
+            sentWeightInKG: 0,
+            spamWeightInKG: 0
         };
 
-        carbonFootprint.inbox =
+        carbonFootprint.inboxWeightInKG =
             ((this.emailCounts[EmailType.ShortEmailOnPhone] || 0) * EmissionsInGramsPerEmail[EmailType.ShortEmailOnPhone] +
                 (this.emailCounts[EmailType.ShortEmailOnLaptop] || 0) * EmissionsInGramsPerEmail[EmailType.ShortEmailOnLaptop] +
                 (this.emailCounts[EmailType.LongEmailOnLaptop] || 0) * EmissionsInGramsPerEmail[EmailType.LongEmailOnLaptop] +
                 (this.emailCounts[EmailType.EmailBlast] || 0) * EmissionsInGramsPerEmail[EmailType.EmailBlast]) /
             1000;
 
-        carbonFootprint.sent =
+        carbonFootprint.sentWeightInKG =
             ((this.emailCounts[EmailType.SentEmail] || 0) * EmissionsInGramsPerEmail[EmailType.SentEmail]) / 1000;
 
-        carbonFootprint.spam =
+        carbonFootprint.spamWeightInKG =
             ((this.emailCounts[EmailType.SpamEmail] || 0) * EmissionsInGramsPerEmail[EmailType.SpamEmail]) / 1000;
 
         return carbonFootprint;
@@ -59,9 +59,9 @@ class getCarbonFootprint{
         return `
         emailId : ${this.entity.email}
         source : ${this.entityType[0]}
-        inbox : ${carbonFootprint.inbox.toFixed(2)} KG
-        sent : ${carbonFootprint.sent.toFixed(2)} KG
-        spam : ${carbonFootprint.spam.toFixed(2)} KG
+        inbox : ${carbonFootprint.inboxWeightInKG.toFixed(2)} KG
+        sent : ${carbonFootprint.sentWeightInKG.toFixed(2)} KG
+        spam : ${carbonFootprint.spamWeightInKG.toFixed(2)} KG
         `;
     }
 }
@@ -73,7 +73,7 @@ let entity = { email: "purviharpalani@gmail.com" };
 const emailCounts: Record<EmailType, number> = {
     [EmailType.SpamEmail]: 2000,
     [EmailType.SentEmail]: 450,
-    [EmailType.ShortEmailOnPhone]: 300,g
+    [EmailType.ShortEmailOnPhone]: 300,
     [EmailType.ShortEmailOnLaptop]: 240,
     [EmailType.LongEmailOnLaptop]: 10,
     [EmailType.EmailBlast]: 3
