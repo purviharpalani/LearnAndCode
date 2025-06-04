33a7655e976c2213493f47fbf8d894cece18686e
Purvi Harpalani <purvi.harpalani@tamp.co.in>
2025-06-04 23:36:00 +0530

    Added code in divisorChecker.ts to pass the test case

--- Ch-9_Unit_Tests/src/divisorChecker.ts
+++ Ch-9_Unit_Tests/src/divisorChecker.ts
@@ -1,3 +1,19 @@
+function countDivisors(n: number): number {
+  let count = 0;
+  for (let i = 1; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      count += i * i === n ? 1 : 2;
+    }
+  }
+  return count;
+}
+
 export function countConsecutiveMatchesWithSameDivisors(k: number): number {
-  throw new Error("not implemented yet");
+  let matchCount = 0;
+  for (let n = 2; n <= k; n++) {
+    if (countDivisors(n) === countDivisors(n - 1)) {
+      matchCount++;
+    }
+  }
+  return matchCount;
 }
